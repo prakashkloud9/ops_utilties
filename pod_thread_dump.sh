dc230e68eb602af281be65a6c5f93b3af5f4732c
prakashkloud9 <34976570+prakashkloud9@users.noreply.github.com>
2018-02-28 21:37:19 +0530

    Create pod_thread_dump.sh

---
+++ pod_thread_dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#podname=$1
+namespace=$1
+region=$2
+#a=(`kubectl get ns |awk '{if(NR>1)print}'`)
+
+podarr=(`kubectl get po -n $namespace |awk '{if(NR>1)print}'| awk {'print $1'}`)
+
+for pod_name in "${podarr[@]:1}"
+do
+kubectl exec  $pod_name --namespace=$namespace -- bash -c "pid=\$(ps -aux|grep java|grep /kohls/config/application/ | awk '{print \$2}'|sed -n 1p) && \
+jstack -l \$pid > threaddump_$pod_name.txt"
+kubectl cp $namespace/$pod_name:/threaddump_$pod_name.txt ./threaddump_$pod_name\_$region.txt 
+echo $pod_name
+done
