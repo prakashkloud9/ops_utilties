550910ccbf9e3da68accee5c13ebd4124206e08d
prakashkloud9 <34976570+prakashkloud9@users.noreply.github.com>
2018-02-28 21:56:41 +0530

    Update pod_thread_dump.sh

--- pod_thread_dump.sh
+++ pod_thread_dump.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#podname=$1
 namespace=$1
 region=$2
-#a=(`kubectl get ns |awk '{if(NR>1)print}'`)
-
 podarr=(`kubectl get po -n $namespace |awk '{if(NR>1)print}'| awk {'print $1'}`)
-
 for pod_name in "${podarr[@]:1}"
 do
 kubectl exec  $pod_name --namespace=$namespace -- bash -c "pid=\$(ps -aux|grep java|grep /kohls/config/application/ | awk '{print \$2}'|sed -n 1p) && \
